12034f8e1f82e5103afcc091e6ea9f46ee7e4ada
Romualdo Dasig <dasig.rg@a5project.com>
2014-11-02 17:35:40 +0800

    Update Utf8 line 41 to work with PHPUnit

--- system/core/Utf8.php
+++ system/core/Utf8.php
@@ -38,7 +38,7 @@ class CI_Utf8 {
 	{
 		log_message('debug', "Utf8 Class Initialized");
 
-		global $CFG;
+		$CFG =& load_class('Config', 'core'); // global $CFG;
 
 		if (
 			preg_match('/./u', 'é') === 1					// PCRE must support UTF-8
